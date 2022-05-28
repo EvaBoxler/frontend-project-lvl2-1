6b30c84694c93f4bdcf561f272b695a5160590f0
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-05-28 16:13:05 +0400

    add .action to geeniff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .argument('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2));
+  });
  
 program.parse();
