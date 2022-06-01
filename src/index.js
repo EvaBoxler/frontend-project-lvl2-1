1a15f133415faed84806ee144853415e946c31bd
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-06-01 15:09:00 +0400

    finished step 3

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import _ from 'lodash';
-import getPath from './getPath.js';
-const genDiff = (filepath1, filepath2) => {
-    const file1 = getPath(filepath1);
-    const file2 = getPath(filepath2);
-    const keys1 = Object.keys(file1);
-    const keys2 = Object.keys(file2);
-    const sortedKeys = _.union(keys1, keys2).sort();
+import readFile from './getPath.js';
 
-    const result = [];
-    sortedKeys.map((key) => {
-        if (!_.has(file1, key)) {
-            result.push(` + ${key}: ${file2[key]}`);
-        } else if (!_.has(file2,key)) {
-            result.push(` - ${key}: ${file1[key]}`);
-        } else if (file1[key] !== file2[key]) {
-            result.push(` - ${key}: ${file1[key]}`);
-            result.push(` + ${key}: ${file2[key]}`);
-        } else {
-            result.push(`   ${key}: ${file1[key]}`);
-        }
-        return null;
-    });
-    
-    return ['{', ...result, '}'].join('\n');
+const genDiff = (filepath1, filepath2) => {
+  const file1 = readFile(filepath1);
+  const file2 = readFile(filepath2);
+  const keys1 = _.keys(file1);
+  const keys2 = _.keys(file2);
+  const sortedKeys = _.union(keys1, keys2).sort();
+
+  const newArr = [];
+  sortedKeys.map((key) => {
+    if (!_.has(file1, key)) {
+      newArr.push(`  + ${key}: ${file2[key]}`);
+    } else if (!_.has(file2, key)) {
+      newArr.push(`  - ${key}: ${file1[key]}`);
+    } else if (file1[key] !== file2[key]) {
+      newArr.push(`  - ${key}: ${file1[key]}`);
+      newArr.push(`  + ${key}: ${file2[key]}`);
+    } else {
+      newArr.push(`    ${key}: ${file1[key]}`);
+    }
+
+    return null;
+  });
+
+  return ['{', ...newArr, '}'].join('\n');
 };
 
 export default genDiff;
