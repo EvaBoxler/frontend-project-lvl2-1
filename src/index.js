d66035903c5b674d0b2315037e488de78206f29b
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-06-03 16:24:02 +0400

    fix eslintrc.yml

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const genDiff = (filepath1, filepath2) => {
   const keys2 = _.keys(file2);
   const sortedKeys = _.union(keys1, keys2).sort();
 
-  let newArr = [];
+  const newArr = [];
   sortedKeys.map((key) => {
     if (!_.has(file1, key)) {
       newArr.push(`  + ${key}: ${file2[key]}`);
