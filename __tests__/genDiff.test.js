f1e4a2a4b9e9f9bf685f4ecadffd07d923c42d5e
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-06-05 15:42:39 +0400

    add node_options

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,16 @@
+import { dirname, resolve } from 'path';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
+
+const expectedOutput = readFile('flatOutput.txt');
+
+test('gediff test', () => {
+  expect(genDiff('file1.json', 'file2.json')).toEqual(expectedOutput);
+});
