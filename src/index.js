a9525881d4e816bf5e04f09dbb63a13ce85333d1
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-06-09 22:23:16 +0400

    work on step 6

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import { extname } from 'path';
-import _ from 'lodash';
 import readFile from './readFile.js';
 import parse from './parsers.js';
+import makeTree from './makeTree.js';
+import stylish from './formatters/stylish.js';
 
 const genDiff = (filepath1, filepath2) => {
   const readFile1 = readFile(filepath1);
   const readFile2 = readFile(filepath2);
   const file1 = parse(readFile1, extname(filepath1));
   const file2 = parse(readFile2, extname(filepath2));
-  const keys1 = _.keys(file1);
-  const keys2 = _.keys(file2);
-  const sortedKeys = _.union(keys1, keys2).sort();
 
-  const newArr = [];
-  sortedKeys.map((key) => {
-    if (!_.has(file1, key)) {
-      newArr.push(`  + ${key}: ${file2[key]}`);
-    } else if (!_.has(file2, key)) {
-      newArr.push(`  - ${key}: ${file1[key]}`);
-    } else if (file1[key] !== file2[key]) {
-      newArr.push(`  - ${key}: ${file1[key]}`);
-      newArr.push(`  + ${key}: ${file2[key]}`);
-    } else {
-      newArr.push(`    ${key}: ${file1[key]}`);
-    }
+  const tree = makeTree(file1, file2);
 
-    return null;
-  });
-
-  return ['{', ...newArr, '}'].join('\n');
+  return stylish(tree);
 };
 
 export default genDiff;
