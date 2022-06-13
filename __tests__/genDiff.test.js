e23090150a9cc7ea3d6d19e7fd263064da4f6ad7
Zaychikov Maks <zaychikov.maks@gmail.com>
2022-06-13 12:08:29 +0400

    add new tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,6 +4,7 @@ import genDiff from '../src/index.js';
 
 const expectedStylishOutput = readFile('stylishOutput.txt');
 const expectedPlainOutput = readFile('plainOutput.txt');
+const expectedJsonOutput = readFile('jsonOutput.txt');
 
 test('gediff stylish test .JSON', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(expectedStylishOutput);
@@ -19,4 +20,12 @@ test('gendiff stylish test .yml', () => {
 
 test('gendiff plain test .yml', () => {
   expect(genDiff('file1.yml', 'file2.yaml', 'plain')).toEqual(expectedPlainOutput);
+});
+
+test('gendiff .json test to JSON', () => {
+  expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(expectedJsonOutput);
+});
+
+test('gendiff .yml and yaml test to JSON', () => {
+  expect(genDiff('file1.yml', 'file2.yaml', 'json')).toEqual(expectedJsonOutput);
 });
